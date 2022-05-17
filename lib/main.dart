a936c23d28c61092256a3013f1006b4b5ea4fa18
DHAVAL AJUDIYA <95960258+DhavalAjudiya@users.noreply.github.com>
2022-05-17 17:59:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ import 'package:dmeo1/view/ragitrastion_screen.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
